e55ce0707bf3a8deba96a567b1bd03e681f08c33
Farzad Slamdien <farzad@slamdien.co.za>
2020-07-22 18:11:28 +0200

    general styling

--- src/App.js
+++ src/App.js
@@ -3,27 +3,15 @@ import logo from './logo.svg';
 import './App.scss';
 import Header from "./components/header";
 import Footer from './components/footer';
+import Jobs from "./containers/jobs";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        <Footer/>
-    </div>
+      <React.Fragment>
+          <Header />
+          <Jobs />
+          <Footer/>
+      </React.Fragment>
   );
 }
 
