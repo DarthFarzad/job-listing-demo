f10622c000baf3c49b2489ba03e3294313347e0c
Farzad Slamdien <farzad@slamdien.co.za>
2020-07-22 18:11:16 +0200

    header
    header images

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,9 @@
 import React from "react";
+import './header.scss';
 
-const Header = () => (<header>Show the header here </header>);
+const Header = () => (<header>
+    <img src="/images/bg-header-mobile.svg" alt="" className="d-md-none"/>
+    <img src="/images/bg-header-desktop.svg" alt="" className="d-none d-md-block"/>
+</header>);
 
 export default Header;
