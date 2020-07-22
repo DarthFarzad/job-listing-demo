6d8208c50a4f5bf36728e40db74e552cb801b43e
Farzad Slamdien <farzad@slamdien.co.za>
2020-07-22 18:11:56 +0200

    job listing
    initial commit

---
+++ src/containers/jobs/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Jobs extends React.PureComponent {
+    render() {
+        return <div className="container">list of content</div>
+    }
+}
+
+export default Jobs;
