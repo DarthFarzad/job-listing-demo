999204e655d0b34fbf1a08c977868366375786e6
Farzad Slamdien <farzad@slamdien.co.za>
2020-07-23 16:46:59 +0200

    scaffolding
    added the scaffolding for most components

--- src/containers/jobs/index.js
+++ src/containers/jobs/index.js
@@ -1,8 +1,31 @@
 import React from "react";
+import Card from "../../components/card";
+
+const sample = {
+    "id": 1,
+    "company": "Photosnap",
+    "logo": "./images/photosnap.svg",
+    "new": true,
+    "featured": true,
+    "position": "Senior Frontend Developer",
+    "role": "Frontend",
+    "level": "Senior",
+    "postedAt": "1d ago",
+    "contract": "Full Time",
+    "location": "USA Only",
+    "languages": [
+        "HTML",
+        "CSS",
+        "JavaScript"
+    ],
+    "tools": []
+};
 
 class Jobs extends React.PureComponent {
     render() {
-        return <div className="container">list of content</div>
+        return (<div className="container">
+            <Card {...sample} />
+        </div>);
     }
 }
 
