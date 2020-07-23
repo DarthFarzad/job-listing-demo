c05b5be134e77d4a25b5741902fc1c7af59fc2b7
Farzad Slamdien <farzad@slamdien.co.za>
2020-07-23 19:18:02 +0200

    reducer
    reducers, initial actions and filtering

--- src/containers/jobs/index.js
+++ src/containers/jobs/index.js
@@ -1,32 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../../App";
 import Card from "../../components/card";
 
-const sample = {
-    "id": 1,
-    "company": "Photosnap",
-    "logo": "./images/photosnap.svg",
-    "new": true,
-    "featured": true,
-    "position": "Senior Frontend Developer",
-    "role": "Frontend",
-    "level": "Senior",
-    "postedAt": "1d ago",
-    "contract": "Full Time",
-    "location": "USA Only",
-    "languages": [
-        "HTML",
-        "CSS",
-        "JavaScript"
-    ],
-    "tools": []
-};
-
-class Jobs extends React.PureComponent {
-    render() {
-        return (<div className="container">
-            <Card {...sample} />
-        </div>);
-    }
+export default function Jobs() {
+    const [data] = useContext(Context);
+    return (<div className="container">
+        {data.jobs.map(job => <Card key={job.id} {...job}/>)}
+    </div>);
 }
-
-export default Jobs;
