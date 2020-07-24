5a1db453628c1d0cd49dd5182dc8f084f00b9cbc
Farzad Slamdien <farzad@slamdien.co.za>
2020-07-24 09:51:01 +0200

    Updates

---
+++ precache-manifest.af90870831f51d2ad5be387cbb833d00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b007d9f435e740cd631488c30ed3020",
+    "url": "/job-listing-demo/index.html"
+  },
+  {
+    "revision": "6cf077979534547c8606",
+    "url": "/job-listing-demo/static/css/main.bac2feb0.chunk.css"
+  },
+  {
+    "revision": "614e4d5ad9682083fb70",
+    "url": "/job-listing-demo/static/js/2.7f56d668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-listing-demo/static/js/2.7f56d668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf077979534547c8606",
+    "url": "/job-listing-demo/static/js/main.c1677d73.chunk.js"
+  },
+  {
+    "revision": "0c44191c018e589f2f85",
+    "url": "/job-listing-demo/static/js/runtime-main.01fd3449.js"
+  }
+]);
