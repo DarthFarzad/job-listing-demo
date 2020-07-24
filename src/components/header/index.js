cd7e80d11cac502e2b6fca72738085f0339d6a1d
Farzad Slamdien <farzad@slamdien.co.za>
2020-07-24 10:46:29 +0200

    image path
    fixed relative image path error

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import './header.scss';
 
 const Header = () => (<header>
-    <img src="/images/bg-header-mobile.svg" alt="" className="d-md-none"/>
-    <img src="/images/bg-header-desktop.svg" alt="" className="d-none d-md-block"/>
+    <img src="./images/bg-header-mobile.svg" alt="" className="d-md-none"/>
+    <img src="./images/bg-header-desktop.svg" alt="" className="d-none d-md-block"/>
 </header>);
 
 export default Header;
