06d888036ffd27cf68d40e668bcc91745aa377af
Farzad Slamdien <farzad@slamdien.co.za>
2020-07-24 10:33:21 +0200

    styling fixes
    changed some inconsistencies in the styling

--- src/containers/jobs/index.js
+++ src/containers/jobs/index.js
@@ -3,6 +3,7 @@ import { Context } from "../../App";
 import Card from "../../components/card";
 
 export default function Jobs() {
-    const [{jobs}] = useContext(Context);
-    return <div className="list">{jobs.map(job => <Card key={job.id} {...job}/>)}</div>;
+    const [{jobs, filters}] = useContext(Context);
+    const filtered = filters.length > 0;
+    return <div className="list" data-filtered={filtered}>{jobs.map(job => <Card key={job.id} {...job}/>)}</div>;
 }
